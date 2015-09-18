52ecd92bccc636e986e92a9ed7cb40bc9dc000d7
Blake Strickland <Stricklandb02@gmail.com>
2015-09-18 14:21:53 -0400

    Initical commit/ first test pass

---
+++ LAV/term.rb
@@ -0,0 +1,15 @@
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'test.sqlite3'
+)
+class Term < ActiveRecord::Base
+  belongs_to :school
+  default_scope { order('ends_on DESC') }
+
+  scope :for_school_id, ->(school_id) { where("school_id = ?", school_id) }
+
+  def school_name
+    school ? school.name : "None"
+  end
+end
