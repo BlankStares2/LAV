5e338c7cf679f8d553bcb1537a540f40aee529ef
Blake Strickland <Stricklandb02@gmail.com>
2015-09-20 21:39:41 -0400

    HIP HIOP HORRAYYYY

---
+++ term.rb
@@ -0,0 +1,21 @@
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'test.sqlite3'
+)
+class Term < ActiveRecord::Base
+  has_many :courses, dependent: :restrict_with_error
+  belongs_to :school
+  default_scope { order('ends_on DESC') }
+
+  scope :for_school_id, ->(school_id) { where("school_id = ?", school_id) }
+
+  def add_course(c)
+    courses << c
+  end
+
+  def school_name
+    school ? school.name : "None"
+  end
+end
+ 
